c4d50688ac03e1cc1b7ecb59a242d06fabe02ae3
wajih <wajih.ben-ali@daveo.fr>
2018-07-26 16:47:02 +0200

    changement webhook

--- index.js
+++ index.js
@@ -4,21 +4,15 @@ let bodyParser = require('body-parser')
 
 // These two following lines ensures that every incomming request
 // is parsed to json automatically
-app.use(bodyParser.urlencoded({ extended: 'true' }))
-app.use(bodyParser.json())
 
 // Allow access to resources from any origin and any headers. As we want
 // the agent to reach the webhook and not bother with CORS, they are fully
 // permissive
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+
 
 /***** NEW *****/
 // Handle POST http requests on the /webhook endpoint
-app.post('/webhook', webhook)
+
 
 const webhook = (req, res) => {
   let body = req.body
@@ -37,5 +31,4 @@ source:"webhook"
   // Sending back the results to the agent
   res.json(response)
 }
-
-module.exports = webhook
+app.post('/webhook', webhook)
