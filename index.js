90097e2ccce84d4f6de4b856b545a5c1e2b2c7c0
wajih <wajih.ben-ali@daveo.fr>
2018-07-26 14:50:52 +0200

    changement d'index

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-app.post('/webhook',function (req, res){
-console.log('Received a post request');
-if(!req.body) return res.sendStatus(400)
-res.setHeader('Content-Type','application/json');
-console.log('here is the post request from dialogflow');
-console.log(req.body);
 
-let response= " ";
-let responseObj={
-  "fulfillmentText":response,
-  "source":"webhook node"}
-  return res.json(responseObj);
+const webhook = (req, res) => {
+  let body = req.body
 
-})
+  // Retrieving parameters from the request made by the agent
+  let action = body.result.action
+  let parameters = body.result.parameters
+
+  // Performing the action
+  response.fulfillmentText="helo";
+  response.source="webhook";
+  
+
+  // Sending back the results to the agent
+  res.json(response)
+}
+
+module.exports = webhook
